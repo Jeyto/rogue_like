d77008f5035d8188f8261facb00939dc8537739c
Maxime <gerbault.maxime@gmail.com>
2015-11-27 13:21:41 +0100

    Version 1.2

--- Joueur/deplacement.c
+++ Joueur/deplacement.c
@@ -1,27 +1,49 @@
 #include <ncurses.h>
 #include <unistd.h>
-#define DELAY 30000
+#include <string.h>
 
 int main(void) {
-  int x = 0, y = 0, y_init=0;
-  
+  int x = 0, y = 0, y_init=0, ch, row, col;
+ 
+  char str[80];
+  char mesg[]="o";
   initscr();
   noecho();
   curs_set(FALSE);
-  
+  keypad(stdscr, TRUE);
+  cbreak();
+
   while(1) {
-    clear();             // Clear the screen of all
-                         // previously-printed characters
-    mvprintw(y, x, "o"); // Print our "ball" at the current xy position
-    refresh();
-    usleep(DELAY);       // Shorter delay between movements
-    x++;		// Advance the ball to the right
-    y++;
-    
-    if (y==20) y=++y_init;
-        if (x==20) { x=0;  }
-    		
-  }
-  
-  endwin();
+	clear();
+	getmaxyx(stdscr,row,col); /* get the number of rows and columns */	
+ 	
+ 	
+ 	if((x >= 0) && (x < col) && (y >= 0) && (y < row)){
+ 		
+ 		
+ 		mvprintw(y, x, "%s", mesg);
+		ch = getch();
+		switch(ch){
+			case KEY_UP: 
+				y--;
+				break;
+			case KEY_DOWN:
+				y++;
+				break;	
+			case KEY_RIGHT:
+				x++;
+				break;		
+			case KEY_LEFT:
+				x--;
+				break;
+		}					
+	}
+	else if(y < 0 ) y = row-1;
+	else if(x < 0 ) x = col-1; 
+	else if(y > row-1 ) y = 0;
+	else if(x > col-1 ) x = 0;
+   		refresh();
+   	
+ }
+ 	endwin();			/* End curses mode		  */
 }
